27a9d7c8f8d7f19329f89ab28d3e7f460c616865
ahig0ne <sebaselnub@gmail.com>
2025-01-30 08:45:41 -0500

    feat : :zap: update

--- src/components/createComponents/createCircuitComponent.js
+++ src/components/createComponents/createCircuitComponent.js
@@ -7,18 +7,18 @@ class CreateCircuit extends HTMLElement{
         this.innerHTML = `
         <div class="formCrear">
             <form id="myformCrearCircuito">
-                <label>Name<input type="text"></label>
-                <label>Country<input type="text"></label>
-                <label>Length<input type="number"></label>
-                <label>Laps <input type="number"></label>
-                <label>Description<input type="text"></label>
-                <label>Image<input type="url"></label>
+                <label>Name<input type="text" name="circuitName"></label>
+                <label>Country<input type="text" name="circuitCountry"></label>
+                <label>Length<input type="number" name="circuitLength"></label>
+                <label>Laps <input type="number" name="circuitLaps"></label>
+                <label>Description<input type="text" name="circuitDescription"></label>
+                <label>Image<input type="url" name="circuitImage"></label>
                 <button type="submit">Submit</button>
             </form>
-        </div>
+        </div>;
         `;
 
-        
+
     }
 }
 
